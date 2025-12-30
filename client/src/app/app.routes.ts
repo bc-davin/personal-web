491b45072070c9797b8e3d1da92679dd0ab3b101
Davin <71451687+bc-davin@users.noreply.github.com>
2025-12-30 11:31:46 -0700

    done

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { BlogComponent } from './blog/blog.component';
-import { BlogDetailComponent } from './blog/blog-detail.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'blog', component: BlogComponent },
-  { path: 'blog/:id', component: BlogDetailComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' }
 ];
