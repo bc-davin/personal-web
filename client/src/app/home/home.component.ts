2b7a83290ffa948958029cea3a33276028a09fdf
Davin <71451687+bc-davin@users.noreply.github.com>
2025-12-30 09:28:38 -0700

    added the home page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { ProjectService } from '../services/project.service';
-import { BlogService } from '../services/blog.service';
-import { Project } from '../models/project.model';
-import { Blog } from '../models/blog.model';
 
 @Component({
   selector: 'app-home',
@@ -12,37 +8,5 @@ import { Blog } from '../models/blog.model';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-  private readonly projectService = inject(ProjectService);
-  private readonly blogService = inject(BlogService);
-
-  protected readonly featuredProjects = signal<Project[]>([]);
-  protected readonly recentBlogs = signal<Blog[]>([]);
-  protected readonly loading = signal(true);
-  protected readonly error = signal<string | null>(null);
-
-  ngOnInit(): void {
-    this.loadFeaturedContent();
-  }
-
-  private loadFeaturedContent(): void {
-    this.loading.set(true);
-    this.error.set(null);
-
-    Promise.all([
-      this.projectService.getAll(true).toPromise(),
-      this.blogService.getAll(true).toPromise()
-    ])
-      .then(([projects, blogs]) => {
-        this.featuredProjects.set(projects?.slice(0, 3) || []);
-        this.recentBlogs.set(blogs?.slice(0, 3) || []);
-      })
-      .catch(err => {
-        console.error('Error loading featured content:', err);
-        this.error.set('Failed to load content. Please try again later.');
-      })
-      .finally(() => {
-        this.loading.set(false);
-      });
-  }
+export class HomeComponent {
 }
